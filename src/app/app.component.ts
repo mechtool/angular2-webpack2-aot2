51731e73186196ba28695f0c50dd8c807510ce69
Dell <farros@rambler.ru>
2017-07-25 10:51:10 +0300

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@
  * location : src/app/app.component.ts
  */
 import { Component } from '@angular/core';
-import '!style-loader!css-loader!sass-loader!../../public/scss/styles.scss';
+import '!style-loader!css-loader!sass-loader!../../public/styles.css';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent { }
